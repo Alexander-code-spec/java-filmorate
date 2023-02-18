3cf7c950fad44139b0aae73446a72c0064cbd339
Alexander <ssddds73@gmail.com>
2023-02-18 23:07:16 +0300

    "fetch: Реалзиован функиционал по режисеру"

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -60,9 +60,13 @@ public class FilmController{
         return filmService.removeLike(filmService.getFilmStorage().get(id), userId);
     }
 
+    @GetMapping("/director/{directorId}")
+    public List<Film> getByDirectorId(@PathVariable("directorId") Integer directorId, @RequestParam String sortBy) {
+        return filmService.getSortDirector(directorId, sortBy);
+    }
+
     @PutMapping
-    @Validated
-    public Film update(@Valid @RequestBody Film film) throws ValidationException {
+    public Film update(@RequestBody @Valid  Film film) throws ValidationException {
         return filmService.getFilmStorage().update(film);
     }
 
